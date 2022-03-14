fc68f5141f1be73a33cec1e59c4e249ba908068d
Grzegorz Sobocinski <grzegorz.sobocinski@pl.abb.com>
2022-03-14 09:25:25 +0100

    Create diagrams demo with custom component

---
+++ DiagramJS/Data/Process.cs
@@ -0,0 +1,19 @@
+﻿using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+
+namespace DiagramJS.Data
+{
+    public class Process : NodeModel
+    {
+        public Process() : base(Point.Zero)
+        {
+        }
+
+        public Process(Point position) : base(position)
+        {
+        }
+
+        public string Segment { get; set; } = string.Empty;
+        public string Material { get; set; } = string.Empty;
+    }
+}
--- DiagramJS/Pages/Index.razor.cs
+++ DiagramJS/Pages/Index.razor.cs
@@ -1,6 +1,41 @@
-﻿namespace DiagramJS.Pages
+﻿using Blazor.Diagrams.Core;
+using Blazor.Diagrams.Core.Geometry;
+using Blazor.Diagrams.Core.Models;
+using DiagramJS.Data;
+
+namespace DiagramJS.Pages
 {
     public partial class Index
     {
+        public Diagram Diagram { get; set; } = new();
+
+        protected override void OnInitialized()
+        {
+            Setup();
+        }
+
+        private void Setup()
+        {
+            Diagram.RegisterModelComponent<Process, CustomNodeComponent>();
+
+            var rootNode = new Process(new Point(50, 50));
+            rootNode.AddPort(PortAlignment.Bottom);
+            rootNode.Material = "SM";
+            rootNode.Title = "Process";
+
+            var childNode = new Process(new Point(50, 300));
+            childNode.AddPort(PortAlignment.Bottom);
+            childNode.AddPort(PortAlignment.Top);
+            rootNode.Material = "Custom";
+            childNode.Title = "Manufacturing";
+
+            var grandChildNode = new Process(new Point(50, 600));
+            grandChildNode.AddPort(PortAlignment.Top);
+            grandChildNode.Title = "Smart";
+
+            Diagram.Nodes.Add(new[] { rootNode, childNode, grandChildNode });
+            Diagram.Links.Add(new LinkModel(rootNode.GetPort(PortAlignment.Bottom), childNode.GetPort(PortAlignment.Top)));
+            Diagram.Links.Add(new LinkModel(childNode.GetPort(PortAlignment.Bottom), grandChildNode.GetPort(PortAlignment.Top)));
+        }
     }
 }
